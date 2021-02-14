e10702c4f8cda7cdb66e40f01128b6d08dbc2dcc
Juan Flores <jcflowres7@gmail.com>
2021-02-13 20:10:43 -0500

    commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var firstName = "Juan";
+var lastName = " Flores"; 
+var age = 21;
+alert("My name is " + firstName + lastName);
+alert("I am " + age + "years old");
+console.log("I am having losts of fun in this class. I can't wait to learn more every week");
